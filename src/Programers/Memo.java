5055c14aaf4f67349e98dde8cd4180f579af98ee
Jung Sung-Min <jsms91@hanmail.net>
2024-10-24 23:06:03 +0900

    테스트커밋

--- src/Programers/Memo.java
+++ src/Programers/Memo.java
@@ -2,6 +2,6 @@ package Programers;
 
 public class Memo {
     public static void main(String[] args) {
-        System.out.println("프로그래머스 코딩테스트 문제풀이");
+        System.out.println("프로그래머스");
     }
 }
